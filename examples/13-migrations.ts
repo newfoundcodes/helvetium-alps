0b7de5ba0bc235be18a94542ffa7b99304082639
Nathanne Isip <nathanne@nthnn.dev>
2026-09-08 00:47:05 +0800

    docs(examples): add migrations example

---
+++ examples/13-migrations.ts
@@ -0,0 +1,54 @@
+/*
+ * Helvetium Alps - Helvetium Framework, A Newfoundcodes project.
+ *
+ * Copyright (C) 2026 Jonathan Eldy Baldivicio
+ *
+ * Author: Jonathan Eldy Baldivicio
+ * Contact: jonathaneldy.baldivicio@newfoundcodes.com
+ *
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+import {
+  Alps,
+  postgres,
+  Migrator,
+  defineMigration,
+  defineTable,
+  types,
+} from '@newfoundcodes/helvetium-alps';
+
+const db = new Alps()
+  .register('main', postgres({ query: async () => ({ rows: [], rowCount: 0 }) }))
+  .sql();
+
+const users = defineTable('users', {
+  id: types.integer({ primaryKey: true }),
+  email: types.string(),
+});
+
+const migrator = new Migrator(db).register(
+  defineMigration('202609040001_users', {
+    up({ schema }) {
+      schema.createTable(users);
+    },
+    down({ schema }) {
+      schema.dropTable('users');
+    },
+  }),
+);
+
+console.log(await migrator.up());
