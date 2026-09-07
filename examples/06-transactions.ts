529c8b193c83be4ac51ffe724ebdb5fb6f0ac3d2
Nathanne Isip <nathanne@nthnn.dev>
2026-09-08 00:47:04 +0800

    docs(examples): add transactions example

---
+++ examples/06-transactions.ts
@@ -0,0 +1,50 @@
+/*
+ * Helvetium Alps - Helvetium Framework, A Newfoundcodes project.
+ *
+ * Copyright (C) 2026 Jonathan Eldy Baldivicio
+ *
+ * Author: Jonathan Eldy Baldivicio
+ * Contact: jonathaneldy.baldivicio@newfoundcodes.com
+ *
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+import { Alps, postgres } from '@newfoundcodes/helvetium-alps';
+
+const log: string[] = [];
+const client = {
+  query: async (text: string) => {
+    log.push(text);
+    return { rows: [], rowCount: 1 };
+  },
+  begin: async () => {
+    log.push('BEGIN');
+  },
+  commit: async () => {
+    log.push('COMMIT');
+  },
+  rollback: async () => {
+    log.push('ROLLBACK');
+  },
+};
+
+const db = new Alps().register('main', postgres(client)).sql();
+await db.transaction(async (tx) => {
+  await tx.query('UPDATE accounts SET balance=balance-100 WHERE id=1');
+  await tx.query('UPDATE accounts SET balance=balance+100 WHERE id=2');
+});
+
+console.log(log);
